9764a8716572a8f78f68a816a320948067070421
olganedelcu <33576634+olganedelcu@users.noreply.github.com>
2019-12-10 00:36:54 +0100

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 
-
+    function myFunction() {
+        var person =prompt("Please enter your name " , " Grinch");
+        if (person !=null) {
+            document.getElementById("demo").innerHTML =
+            "Hello " + person + " .Were you good with your parents?";
+        }else {
+            document.getElementById("")
+        }
+        }
 
 function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
